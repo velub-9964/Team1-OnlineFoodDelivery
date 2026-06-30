b030246a776890a170fd57375a6ab8891485a5df
Devika RK <devika.rk@fadv.com>
2026-06-30 07:44:21 -0700

    Added CartService validation

--- CartService/DTOs/AddCartItemDto.cs
+++ CartService/DTOs/AddCartItemDto.cs
@@ -1,11 +1,21 @@
-﻿namespace CartService.DTOs
-{
-    public class AddCartItemDto
-    {
+﻿using System.ComponentModel.DataAnnotations;
+namespace CartService.DTOs;
 
-        public int CustomerId { get; set; }
-        public int MenuId { get; set; }
-        public int Quantity { get; set; }
-        public decimal Price { get; set; }
-    }
+public class AddCartItemDto
+{
+    [Required]
+    [Range(1, int.MaxValue)]
+
+    public int CustomerId { get; set; }
+
+    [Required]
+    [Range(1, int.MaxValue)]
+    public int MenuId { get; set; }
+    [Required]
+    [Range(1,100)]
+    public int Quantity { get; set; }
+
+    [Required]
+    [Range(0.01, double.MaxValue)]
+    public decimal Price { get; set; }
 }
